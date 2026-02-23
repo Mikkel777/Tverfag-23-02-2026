3a105bdb680aee2d27b2b3e7deebdf7aeb092a11
mikkel777 <mikkelskappel08@icloud.com>
2026-02-23 10:41:24 +0100

    I have created a router with post and get requests, which now makes it possible to create reviews. The reviews are stored in a temp mongodb, and will be displayed on the index site.

--- router/routes.js
+++ router/routes.js
@@ -1,12 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const Review = require('../models/review');
 
-router.get('/', (req, res) => {
-    res.render('index');
+router.get('/', async (req, res) => {
+    try {
+        const reviews = await Review.find().sort({ createdAt: -1 });
+
+        res.render('index', { reviews });
+    } catch (err) {
+        console.error(err);
+        res.send("Database error");
+    }
 });
 
-router.get('/rate', (req, res) => {
-    res.render('rate');
+// Rate routes, get and post
+
+router.get('/rate', async (req, res) => {
+    try {
+        const reviews = await Review.find().sort({createdAt: -1});
+        res.render('rate', { reviews});
+    } catch (err) {
+        console.errror(err);
+        res.send("Db error");
+    }
+});
+
+router.post('/rate', async (req, res) => {
+    try {
+        const { sitename, url, rating, comment, username} = req.body;
+        const newReview = new Review ({
+            sitename,
+            url,
+            rating,
+            comment,
+            username
+        });
+        await newReview.save();
+        res.redirect('/');
+    } catch (err) {
+        console.error(err);
+        res.send("Error while saving the review");
+    }
 });
 
 router.get('/faq', (req, res) => {
